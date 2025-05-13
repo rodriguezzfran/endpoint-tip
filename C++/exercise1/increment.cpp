b4f313232c0d0c1ef693eaccc372085387e18013
Rodriguezzfran <Francorod47@gmail.com>
2025-05-13 12:38:39 -0300

    empty main

--- C++/exercise1/increment.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-// th '&' symbol is used to pass the variable by reference
-void increment(int& num){
-    std::cout << "Incrementing " << num << std::endl;
-    num++;
-}
-
-
--- C++/exercise1/increment.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCREMENT_H
-#define INCREMENT_H
-
-/**
- * @brief Increments the given integer by 1.
- */
-void increment(int& num);
-
-#endif
