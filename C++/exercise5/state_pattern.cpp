46805bafad5f9dd7a3406bb7ee19db7ec35b0dbf
Rodriguezzfran <Francorod47@gmail.com>
2025-05-19 12:19:53 -0300

    added pattern state

---
+++ C++/exercise5/state_pattern.cpp
@@ -0,0 +1,21 @@
+#include "state_pattern.hpp"
+
+int main(){
+    std::shared_ptr<State> offState = std::make_shared<OffState>();
+    FSMcontext fsm(offState);
+
+    fsm.turnOn(); // Off -- TurnOn --> Stopped
+    fsm.speedUp(); // Stopped -- SpeedUp --> Walking
+    fsm.speedUp(); // Walking -- SpeedUp --> Running
+    fsm.turnOff(); // Stopped -- TurnOff --> Off
+    fsm.turnOn(); // Off -- TurnOn --> Stopped
+    fsm.speedDown(); // Stopped -- SpeedDown --> Error
+    fsm.turnOff(); // Error -- TurnOff --> Off
+    fsm.turnOn(); // Off -- TurnOn --> Stopped
+    fsm.speedUp(); // Stopped -- SpeedUp --> Walking
+    fsm.speedUp(); // Walking -- SpeedUp --> Running
+    fsm.speedUp(); // Running -- SpeedUp --> Error
+    fsm.turnOff(); // Error -- TurnOff --> Off
+
+    return 0;
+}
--- C++/exercise5/state_pattern.hpp
+++ C++/exercise5/state_pattern.hpp
@@ -1,6 +1,145 @@
 #ifndef STATE_PATTERN_HPP
 #define STATE_PATTERN_HPP
 
+#include <string>
+#include <iostream>
+#include <memory>
 
+class FSMcontext;
+class OffState;
+class StoppedState;
+class WalkingState;
+class RunningState;
+class ErrorState;
+
+class State {
+    protected:
+        FSMcontext* context;
+    public:
+        virtual ~State() = default;
+        void setContext(FSMcontext* context) {
+            this->context = context;
+        }
+        virtual void turnOn() = 0;
+        virtual void turnOff() = 0;
+        virtual void speedUp() = 0;
+        virtual void speedDown() = 0;
+};
+
+class FSMcontext {
+    private:
+        std::shared_ptr<State> currentState;
+
+    public:
+        FSMcontext(std::shared_ptr<State> initialState) {
+            changeState(initialState);
+        }
+
+        void changeState(std::shared_ptr<State> newState) {
+            currentState = newState;
+            currentState->setContext(this);
+        }
+
+        void turnOn() {
+            currentState->turnOn();
+        }
+
+        void turnOff() {
+            currentState->turnOff();
+        }
+
+        void speedUp() {
+            currentState->speedUp();
+        }
+
+        void speedDown() {
+            currentState->speedDown();
+        }
+
+};
+
+class OffState : public State {
+    public:
+        void turnOn() override {
+            std::cout << "Off -- TurnOn --> Stopped\n";
+            context->changeState(std::static_pointer_cast<State>(std::make_shared<StoppedState>()));
+        }
+
+        void turnOff() override {}
+        void speedUp() override {}
+        void speedDown() override {}
+};
+
+class StoppedState : public State {
+    public:
+        void turnOn() override {
+        }
+
+        void turnOff() override {
+            std::cout << "Stopped -- TurnOff --> Off\n";
+            context->changeState(std::static_pointer_cast<State>(std::make_shared<OffState>()));
+        }
+
+        void speedUp() override {
+            std::cout << "Stopped -- SpeedUp --> Walking\n";
+            context->changeState(std::static_pointer_cast<State>(std::make_shared<WalkingState>()));
+        }
+        void speedDown() override {
+            std::cout << "Stopped -- SpeedDown --> Error\n";
+            context->changeState(std::static_pointer_cast<State>(std::make_shared<ErrorState>()));
+        }
+};
+
+class WalkingState : public State {
+    public:
+        void turnOn() override {}
+        void turnOff() override {
+            std::cout << "Walking -- TurnOff --> Off\n";
+            context->changeState(std::make_shared<OffState>());
+        }
+
+        void speedUp() override {
+            std::cout << "Walking -- SpeedUp --> Running\n";
+            context->changeState(std::static_pointer_cast<State>(std::make_shared<RunningState>()));
+        }
+
+        void speedDown() override {
+            std::cout << "Walking -- SpeedDown --> Stopped\n";
+            context->changeState(std::static_pointer_cast<State>(std::make_shared<StoppedState>()));
+        }
+};
+
+class RunningState : public State {
+    public:
+        void turnOn() override {}
+        void turnOff() override {
+            std::cout << "Running -- TurnOff --> Off\n";
+            context->changeState(std::make_shared<OffState>());
+        }
+
+        void speedUp() override {
+            std::cout << "Running -- SpeedUp --> Error\n";
+            context->changeState(std::static_pointer_cast<State>(std::make_shared<ErrorState>()));
+
+        }
+
+        void speedDown() override {
+            std::cout << "Running -- SpeedDown --> Walking\n";
+            context->changeState(std::make_shared<StoppedState>());
+        }
+        
+};
+
+class ErrorState : public State {
+    public:
+        void turnOn() override {}
+        void turnOff() override {
+            std::cout << "Error -- TurnOff --> Off\n";
+            context->changeState(std::make_shared<OffState>());
+        }
+
+        void speedUp() override {}
+        void speedDown() override {}
+}; 
 
 #endif
